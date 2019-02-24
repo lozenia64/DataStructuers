39a14556b8111ca50a4fd8d1146cb7e65be5d9b0
lozenia64 <lozenia64@gmail.com>
2019-02-24 13:02:55 +0900

    3 배열

---
+++ 2.ArraysAndStructures/004.dynamicArray_1D.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MALLOC(p,s) \
+    if ((p = malloc(sizeof(s))) == NULL) { \
+        fprintf(stderr, "Insufficient memory"); \
+		exit(EXIT_FAILURE); \
+    }
+
+int main(){
+	int i, n, *Arr;
+	printf("Enter the number of numbers to generate : ");
+	scanf("%d", &n);
+	if(n < 1){ //n<1 인 경우 프로그램을 종료시킴. 
+		fprintf(stderr, "Improper value of n\n");
+		exit(EXIT_FAILURE);
+	}
+	MALLOC(Arr, n*sizeof(n));
+}
